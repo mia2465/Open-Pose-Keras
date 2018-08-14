cd6dd1b7932a6e1a119ab99928c68fb50fba35dc
Rachit Saluja <rachit.saluja@gmail.com>
2018-08-13 20:02:11 -0400

    Update config_reader_colab.py

--- config_reader_colab.py
+++ config_reader_colab.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def config_reader_colab():
-    config = ConfigObj('config')
+    config = ConfigObj('/content/Open-Pose-Keras/config')
 
     param = config['param']
     model_id = param['modelID']
